8568ff290fa833a6bbc12997e4d13d758217a933
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-10-25 09:35:54 +0200

    Fix JPA sink adapter in Statistics Service Provider - use `@Transactional` - the ID of a customer is always set; check for existence in database instead

--- statistics-service-provider/src/main/java/de/sample/schulung/statistics/persistence/CustomersSinkJpaImpl.java
+++ statistics-service-provider/src/main/java/de/sample/schulung/statistics/persistence/CustomersSinkJpaImpl.java
@@ -4,6 +4,7 @@ import de.sample.schulung.statistics.domain.Customer;
 import de.sample.schulung.statistics.domain.CustomersSinkPort;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Typed;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
@@ -16,16 +17,18 @@ public class CustomersSinkJpaImpl implements CustomersSinkPort {
   private final CustomerEntityRepository repo;
   private final CustomerEntityMapper mapper;
 
+  @Transactional
   @Override
   public void saveCustomer(Customer customer) {
     var customerEntity = mapper.map(customer);
-    if (customerEntity.getUuid() == null) {
+    if (this.repo.count("uuid", customerEntity.getUuid()) == 0) {
       this.repo.persist(customerEntity);
     } else {
       this.repo.getEntityManager().merge(customerEntity);
     }
   }
 
+  @Transactional
   @Override
   public void deleteCustomer(UUID uuid) {
     repo.deleteById(uuid);
